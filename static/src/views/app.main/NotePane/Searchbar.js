3d85aaf44c4d6ccdf95f3ee569de3ed2791c47ac
Kier Borromeo <seraphipod@gmail.com>
2018-01-26 01:45:45 +0800

    Design update

--- static/src/views/app.main/NotePane/Searchbar.js
+++ static/src/views/app.main/NotePane/Searchbar.js
@@ -1,14 +1,37 @@
 import React from 'react'
-import {Div, Input} from 'glamorous'
+import {Div, Input, Button} from 'glamorous'
 
 class Searchbar extends React.Component {
   render() {
     return (
       <Div
+        flexShrink="0"
         display="flex"
         padding="16"
-        borderBottom="1px solid var(--color-gray)">
-        Super Pogi
+        borderBottom="1px solid var(--color-silver)">
+        <Input display="block"
+          height="var(--form-size)"
+          paddingLeft="8px"
+          paddingRight="8px"
+          marginRight="16px"
+          width="100%"
+          borderRadius="var(--border-radius)"
+          border="1px solid var(--color-silver)"
+          outline="0"
+          placeholder="Search notes..." />
+
+        <Button
+          flexShrink="0"
+          display="inline-block"
+          padding="0"
+          background="transparent"
+          border="0"
+          outline="0"
+          color="var(--color-silver)"
+          cursor="pointer"
+          title="Create new note">
+          <i className="fa fa-pencil-square-o" />
+        </Button>
       </Div>
     )
   }
--- static/src/views/app.main/NotePane/index.js
+++ static/src/views/app.main/NotePane/index.js
@@ -6,59 +6,66 @@ class NotePane extends React.Component {
   render() {
     return (
       <Div
+        display="flex"
+        flexDirection="column"
         height="100%"
         width="280px"
-        borderRight="1px solid var(--color-gray)"
-        overflowY="auto">
+        borderRight="1px solid var(--color-silver)">
         <Searchbar />
-        {new Array(10).fill(null).map((note, i) =>
-          <A
-            display="flex"
-            alignItems="center"
-            height="120px"
-            paddingTop="16px"
-            paddingLeft="16px"
-            width="100%"
-            textDecoration="none"
-            href="#">
-            <Div
-              alignSelf="flex-start"
-              flexShrink="0"
-              marginRight="16px"
-              width="32px"
-              textAlign="center">
+
+        <Div
+          height="100%"
+          overflowY="auto">
+          {new Array(10).fill(null).map((note, i) =>
+            <A
+              display="flex"
+              alignItems="center"
+              height="120px"
+              paddingTop="16px"
+              paddingLeft="16px"
+              width="100%"
+              textDecoration="none"
+              href="#">
               <Div
-                marginBottom="16"
-                color="var(--color-gray)">
-                1h
+                alignSelf="flex-start"
+                flexShrink="0"
+                marginRight="16px"
+                width="32px"
+                textAlign="center">
+                <Div
+                  marginBottom="16"
+                  color="var(--color-silver)">
+                  1h
+                </Div>
+
+                {i < 2 && <Div color="var(--color-red)">
+                  <i className="fa fa-thumb-tack" />
+                </Div>}
               </Div>
 
-              <Div color="var(--color-red)">
-                <i className="fa fa-thumb-tack" />
+              <Div
+                paddingBottom="16px"
+                paddingRight="16px"
+                borderBottom={i === 9 ? '' : '1px solid var(--color-silver)'}>
+                <H4
+                  fontSize="16px"
+                  marginTop="0"
+                  marginBottom="8px"
+                  color="var(--color-black)">
+                  Grocery List
+                </H4>
+
+                <P
+                  margin="0"
+                  lineHeight="1.5"
+                  color="var(--color-dark-silver)">
+                  Bear is the most elegant way to compose, sync,
+                  and store your notes.
+                </P>
               </Div>
-            </Div>
-
-            <Div
-              paddingBottom="16px"
-              paddingRight="16px"
-              borderBottom="1px solid var(--color-gray)">
-              <H4
-                marginTop="0"
-                marginBottom="8px"
-                color="var(--color-black)">
-                Grocery List
-              </H4>
-
-              <P
-                margin="0"
-                lineHeight="1.5"
-                color="var(--color-gray)">
-                Bear is the most elegant way to compose, sync,
-                and store your notes.
-              </P>
-            </Div>
-          </A>
-        )}
+            </A>
+          )}
+        </Div>
       </Div>
     )
   }
