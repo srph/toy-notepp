903c2721a6e7c9de91ff88d0608b0790c8bdbbf1
Kier Borromeo <seraphipod@gmail.com>
2018-01-26 03:08:21 +0800

    Code update

--- static/src/views/app.main/NotePane/index.js
+++ static/src/views/app.main/NotePane/index.js
@@ -1,99 +1,28 @@
 import React from 'react'
 import {Div, A, Input, H4, P} from 'glamorous'
 import Searchbar from './Searchbar'
+import NoteList from './NoteList'
 
 class NotePane extends React.Component {
   render() {
     return (
-      <Div
-        display="flex"
-        flexDirection="column"
-        height="100%"
-        width="280px"
-        borderRight="1px solid var(--color-silver)">
+      <UiWrapper>
         <Searchbar />
-
-        <Div
-          height="100%"
-          overflowY="auto">
-          {new Array(10).fill(null).map((note, i) =>
-            <A
-              position="relative"
-              display="flex"
-              alignItems="center"
-              height="120px"
-              paddingTop="16px"
-              paddingLeft="16px"
-              opacity={i === 0 ? '1': '0.7'}
-              width="100%"
-              textDecoration="none"
-              css={{
-                ':hover': {
-                  opacity: '1'
-                }
-              }}
-              key={i}
-              href="#">
-              {i === 0 && <Div
-                position="absolute"
-                left="0"
-                top="0"
-                bottom="0"
-                backgroundColor="var(--color-blue)"
-                height="100%"
-                width="4px" />}
-
-              {i === 0 && <Div
-                position="absolute"
-                left="56px"
-                bottom="-1px"
-                backgroundColor="var(--color-blue)"
-                height="1px"
-                width="50%" />}
-
-              <Div
-                alignSelf="flex-start"
-                flexShrink="0"
-                marginRight="16px"
-                width="32px"
-                textAlign="center">
-                <Div
-                  marginBottom="16"
-                  color="var(--color-silver)">
-                  1h
-                </Div>
-
-                {i < 2 && <Div color="var(--color-blue)">
-                  <i className="fa fa-thumb-tack" />
-                </Div>}
-              </Div>
-
-              <Div
-                paddingBottom="16px"
-                paddingRight="16px"
-                borderBottom={i === 9 ? '' : '1px solid var(--color-silver)'}>
-                <H4
-                  fontSize="16px"
-                  marginTop="0"
-                  marginBottom="8px"
-                  color="var(--color-black)">
-                  Grocery List
-                </H4>
-
-                <P
-                  margin="0"
-                  lineHeight="1.5"
-                  color="var(--color-dark-silver)">
-                  Bear is the most elegant way to compose, sync,
-                  and store your notes.
-                </P>
-              </Div>
-            </A>
-          )}
-        </Div>
-      </Div>
+        <NoteList />
+      </UiWrapper>
     )
   }
+}
+
+function UiWrapper({children}) {
+  return <Div
+    display="flex"
+    flexDirection="column"
+    height="100%"
+    width="280px"
+    borderRight="1px solid var(--color-silver)">
+    {children}
+  </Div>
 }
 
 export default NotePane
--- static/src/views/app.main/index.js
+++ static/src/views/app.main/index.js
@@ -1,23 +1,71 @@
 import React from 'react'
-import {Div, A, I} from 'glamorous'
-import UiSidebar from './UiSidebar'
-import UiSidebarLink from './UiSidebarLink'
+import {Div, A} from 'glamorous'
 import NotePane from './NotePane'
 
-export default class AppMain extends React.Component {
+class AppMain extends React.Component {
   render() {
     return (
-      <Div
-        display="flex"
-        height="100vh"
-        width="100%">
+      <UiWrapper>
         <UiSidebar>
           <UiSidebarLink icon="book" text="Notebook" active />
           <UiSidebarLink icon="trash" text="Trash" />
         </UiSidebar>
 
         <NotePane />
-      </Div>
+      </UuiWrapper>
     )
   }
 }
+
+function UiWrapper({children}) {
+  return <Div
+    display="flex"
+    height="100vh"
+    width="100%">
+    {children}
+  </Div>
+}
+
+function UiSidebar({children}) {
+  return (
+    <Div
+      height="100%"
+      width="160px"
+      paddingTop="40px"
+      paddingLeft="8px"
+      paddingRight="8px"
+      backgroundColor="var(--color-black)"
+      overflowX="auto">
+      {children}
+    </Div>
+  )
+}
+
+function UiSidebarLink({link, icon, text, active}) {
+  return (
+    <A href="#"
+      display="flex"
+      marginBottom="8px"
+      alignItems="center"
+      padding="8px 16px"
+      fontFamily="var(--font-montserrat)"
+      fontSize="12px"
+      color="var(--color-white)"
+      backgroundColor={active ? 'var(--color-blue)' : 'transparent'}
+      textDecoration="none"
+      borderRadius="var(--border-radius)"
+      transition="200ms all ease"
+      css={{
+        ':hover': {
+          color: active ? '' : 'var(--color-blue)'
+        }
+      }}>
+      <Div marginRight="8px">
+        <i className={`fa fa-${icon}`} />
+      </Div>
+      <span>{text}</span>
+    </A>
+  )
+}
+
+export default AppMain
